59599cdd3ae6e8206de795626dda272513a4351c
pdpan0 <lm.m4rtins@gmail.com>
2022-04-27 09:13:27 -0300

    FEAT: Habilitando OpenFeign para requisições http.

--- src/main/kotlin/com/pdpano/marvelheroes/MarvelHeroesApplication.kt
+++ src/main/kotlin/com/pdpano/marvelheroes/MarvelHeroesApplication.kt
@@ -2,8 +2,10 @@ package com.pdpano.marvelheroes
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class MarvelHeroesApplication
 
 fun main(args: Array<String>) {
